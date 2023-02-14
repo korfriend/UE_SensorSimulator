3e9c3bbe15494ee7f7526358c3e049af2b883a9f
korfriend <korfriend@gmail.com>
2023-02-15 01:20:36 +0900

    project setting up

---
+++ Source/SensorSimulator/Private/SensorSimulatorBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SensorSimulatorBPLibrary.h"
+#include "SensorSimulator.h"
+
+USensorSimulatorBPLibrary::USensorSimulatorBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float USensorSimulatorBPLibrary::SensorSimulatorSampleFunction(float Param)
+{
+	return -1;
+}
+
