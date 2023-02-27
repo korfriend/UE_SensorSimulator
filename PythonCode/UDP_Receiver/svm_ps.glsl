975a247add2e8536b6cce7e26f3a71fb7b94b20f
동준 김 <korfriend@gmail.com>
2023-02-27 18:31:38 +0900

    template glsl code for svm

--- PythonCode/UDP_Receiver/svm_ps.glsl
+++ PythonCode/UDP_Receiver/svm_ps.glsl
@@ -8,17 +8,19 @@ in vec2 texcoord;
 in vec3 worldcoord;
 
 uniform mat4 world2image0;
-//uniform mat4 world2image1;
-//uniform mat4 world2image2;
-//uniform mat4 world2image3;
+uniform mat4 world2image1;
+uniform mat4 world2image2;
+uniform mat4 world2image3;
 
-uniform sampler2D p3d_Texture0;
 uniform sampler2D myTexture0;
 uniform sampler2D myTexture1;
 uniform sampler2D myTexture2;
 uniform sampler2D myTexture3;
 
 void main() {
+    vec4 imagePos = world2image * vec4(worldcoord, 1.0);
+    imagePos.xyz /= imagePos.w;
+    vec2 texPos = (imagePos.xy + vec2(1.0, 1.0)) * 0.5;
     /*
     sampler2D texs[4] = {myTexture0, myTexture1, myTexture2, myTexture3}
     vec4 colors[4];
@@ -45,7 +47,7 @@ void main() {
 
 
 
-    vec4 color = texture(myTexture0, texcoord);
+    vec4 color = texture(myTexture0, texPos);
     p3d_FragColor = color.rgba;
     //p3d_FragColor = texture(p3d_Texture0, texcoord);
     //p3d_FragColor = vec4(texcoord.x, texcoord.y, 0.0, 1.0);
